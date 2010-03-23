e3080af59ecc828468d8ba217c96750cfc14d49a
Gagan Awhad <gaganawhad@gmail.com>
2010-03-23 16:18:06 -0500

    Implemented elementary search functionality

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,6 @@
 class SearchController < ApplicationController
+  def index
+  end
   def label
     matched_ids = LogBookEntry.find_tagged_with(params[:tag]).collect {|e| e.id} 
     @matched_entries = LogBookEntry.find(matched_ids, :joins => [:user, :github_project], :order => params[:order]) 
@@ -6,4 +8,33 @@ class SearchController < ApplicationController
       @tag = params[:tag]
     end
   end
+  
+  def query
+    if params[:query]
+
+      @query = params[:query]
+      @matched_entries = Array.new   
+      @log_book_entries = LogBookEntry.find(:all, :joins => [:user, :github_project]) 
+      
+      
+      @log_book_entries.each do |entry|
+        testString = String.new
+        testString += entry.user.name
+        testString += entry.github_project.github_name
+        testString += entry.github_project.title
+        [:title, :description, :solution, :sha_of_problem, :sha_of_solution ].each do |op|
+          testString += entry.send(op)
+        end
+        
+
+        if testString[@query]
+          @matched_entries << entry #will this matched entries arry be clean to begin with?
+        end
+      end
+    end
+
+
+  end
+
+
 end
