dce7847616c695186a02b5122e24e9b8954b1203
Gagan Awhad <gaganawhad@gmail.com>
2010-03-25 13:21:39 -0500

    created find_by_query in LogBookEntry and moved logic from the query action in Search Controller to the model

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,26 +13,26 @@ class SearchController < ApplicationController
   def query
     #TODO: implement LogBookEntry.find_by_query(params[:query]) in the model
     if params[:query]
-
+      # @matched_entries = LogBookEntry.find_by_query(params[:query])
       @query = params[:query]
-      @matched_entries = Array.new   
-      @log_book_entries = LogBookEntry.find(:all, :joins => [:user, :github_project]) 
-      
-      
-      @log_book_entries.each do |entry|
-        testString = String.new
-        testString += entry.user.name
-        testString += entry.github_project.github_name
-        testString += entry.github_project.title
-        [:title, :description, :solution, :sha_of_problem, :sha_of_solution ].each do |op|
-          testString += entry.send(op)
-        end
-        
-
-        if testString[@query]
-          @matched_entries << entry #will this matched entries arry be clean to begin with?
-        end
-      end
+      @matched_entries = LogBookEntry.find_by_query(params[:query])   
+#      @log_book_entries = LogBookEntry.find(:all, :joins => [:user, :github_project]) 
+#      
+#      
+#      @log_book_entries.each do |entry|
+#        testString = String.new
+#        testString += entry.user.name
+#        testString += entry.github_project.github_name
+#        testString += entry.github_project.title
+#        [:title, :description, :solution, :sha_of_problem, :sha_of_solution ].each do |op|
+#          testString += entry.send(op)
+#        end
+#        
+#
+#        if testString[@query]
+#          @matched_entries << entry #will this matched entries arry be clean to begin with?
+#        end
+#      end
     end
 
 
--- app/models/log_book_entry.rb
+++ app/models/log_book_entry.rb
@@ -4,5 +4,28 @@ class LogBookEntry < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :title, :description, :solution, :user_id, :github_project_id
   validates_uniqueness_of :title
+
+  def self.find_by_query(my_query)
+    matched_entries = Array.new   
+    log_book_entries = LogBookEntry.find(:all, :joins => [:user, :github_project]) 
+    log_book_entries.each do |entry|
+      testString = String.new
+      testString += entry.user.name
+      testString += entry.github_project.github_name
+      testString += entry.github_project.title
+      [:title, :description, :solution, :sha_of_problem, :sha_of_solution ].each do |op|
+        testString += entry.send(op)
+      end
+      
+
+      if testString[my_query]
+        matched_entries << entry #will this matched entries arry be clean to begin with?
+      end
+    end
+     matched_entries
+  end
+
+
+
 end
     
