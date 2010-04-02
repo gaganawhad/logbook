80b7347dc33031fdb4f78891ca7bb42c4f0d56a7
Gagan Awhad <gaganawhad@gmail.com>
2010-04-02 17:30:08 -0500

    removed format html/xml related stuff from controllers that were generated using scaffold

--- app/controllers/github_projects_controller.rb
+++ app/controllers/github_projects_controller.rb
@@ -1,85 +1,45 @@
 class GithubProjectsController < ApplicationController
-  # GET /github_projects
-  # GET /github_projects.xml
   def index
     @github_projects = GithubProject.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @github_projects }
-    end
   end
 
-  # GET /github_projects/1
-  # GET /github_projects/1.xml
   def show
     @github_project = GithubProject.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @github_project }
-    end
   end
 
-  # GET /github_projects/new
-  # GET /github_projects/new.xml
   def new
     @github_project = GithubProject.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @github_project }
-    end
   end
 
-  # GET /github_projects/1/edit
   def edit
     @github_project = GithubProject.find(params[:id])
   end
-
-  # POST /github_projects
-  # POST /github_projects.xml
+  
   def create
     @github_project = GithubProject.new(params[:github_project])
 
-    respond_to do |format|
-      if @github_project.save
-        flash[:notice] = 'GithubProject was successfully created.'
-        format.html { redirect_to(@github_project) }
-        format.xml  { render :xml => @github_project, :status => :created, :location => @github_project }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @github_project.errors, :status => :unprocessable_entity }
-      end
+    if @github_project.save
+      flash[:notice] = 'GithubProject was successfully created.'
+      redirect_to(@github_project)
+    else
+      render :action => "new" 
     end
   end
 
-  # PUT /github_projects/1
-  # PUT /github_projects/1.xml
   def update
     @github_project = GithubProject.find(params[:id])
-
-    respond_to do |format|
-      if @github_project.update_attributes(params[:github_project])
-        flash[:notice] = 'GithubProject was successfully updated.'
-        format.html { redirect_to(@github_project) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @github_project.errors, :status => :unprocessable_entity }
-      end
+      
+    if @github_project.update_attributes(params[:github_project])
+      flash[:notice] = 'GithubProject was successfully updated.'
+      redirect_to(@github_project)
+    else
+      render :action => "edit" 
     end
   end
 
-  # DELETE /github_projects/1
-  # DELETE /github_projects/1.xml
   def destroy
     @github_project = GithubProject.find(params[:id])
     @github_project.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(github_projects_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(github_projects_url)
   end
 end
--- app/controllers/log_book_entries_controller.rb
+++ app/controllers/log_book_entries_controller.rb
@@ -1,89 +1,47 @@
 class LogBookEntriesController < ApplicationController
-  # get /log_book_entries
-  # get /log_book_entries.xml
   def index
     initialize
     @log_book_entries = LogBookEntry.find(:all, :joins => [:user, :github_project], :order => @order).paginate :page => params[:page] 
-
-    
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @log_book_entries }
-    end
   end
 
-  # GET /log_book_entries/1
-  # GET /log_book_entries/1.xml
   def show
     @log_book_entry = LogBookEntry.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @log_book_entry }
-    end
   end
 
-  # GET /log_book_entries/new
-  # GET /log_book_entries/new.xml
   def new
     @log_book_entry = LogBookEntry.new
-
-    respond_to do |format|# gags: This is where control goes to the view. it does work implicty, but it is explicity specified when we want format also in xml.  
-      format.html # new.html.erb
-      format.xml  { render :xml => @log_book_entry }
-    end
-    
-    
   end
-
-  # GET /log_book_entries/1/edit
+    
   def edit
     @log_book_entry = LogBookEntry.find(params[:id])
   end
 
-  # POST /log_book_entries
-  # POST /log_book_entries.xml
   def create
     @log_book_entry = LogBookEntry.new(params[:log_book_entry])
 
-    respond_to do |format|
-      if @log_book_entry.save # Gags: Save validates the entries. 
-        flash[:notice] = 'LogBookEntry was successfully created.'
-        format.html { redirect_to(@log_book_entry) }
-        format.xml  { render :xml => @log_book_entry, :status => :created, :location => @log_book_entry }
-      else # incase of an error in saving
-        format.html { render :action => "new" } # Gags: Render only renders views. it does not call the action new again. 
-        format.xml  { render :xml => @log_book_entry.errors, :status => :unprocessable_entity }
-      end
+    if @log_book_entry.save # Gags: Save validates the entries. 
+      flash[:notice] = 'LogBookEntry was successfully created.'
+      redirect_to(@log_book_entry) 
+    else # incase of an error in saving
+      render :action => "new" 
     end
   end
 
-  # PUT /log_book_entries/1
-  # PUT /log_book_entries/1.xml
   def update
     @log_book_entry = LogBookEntry.find(params[:id])
 
-    respond_to do |format|
-      if @log_book_entry.update_attributes(params[:log_book_entry])
-        flash[:notice] = 'LogBookEntry was successfully updated.'
-        format.html { redirect_to(@log_book_entry) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @log_book_entry.errors, :status => :unprocessable_entity }
-      end
+    if @log_book_entry.update_attributes(params[:log_book_entry])
+      flash[:notice] = 'LogBookEntry was successfully updated.'
+      redirect_to(@log_book_entry)
+    else
+      render :action => "edit" 
     end
   end
 
-  # DELETE /log_book_entries/1
-  # DELETE /log_book_entries/1.xml
   def destroy
     @log_book_entry = LogBookEntry.find(params[:id])
     @log_book_entry.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(log_book_entries_url) }
-      format.xml  { head :ok }
-    end
+    redirect_to(log_book_entries_url) 
   end
 end
+
