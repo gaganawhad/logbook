ea23323efab57a4937e567f738a7f8b9e7a6e311
Gagan Awhad <gaganawhad@gmail.com>
2010-04-08 14:47:57 -0500

    Added comments to models

--- app/models/log_book_entry.rb
+++ app/models/log_book_entry.rb
@@ -1,17 +1,22 @@
 class LogBookEntry < ActiveRecord::Base
+  #Used as a part of adding tags. 
   acts_as_taggable
+  
   belongs_to :github_project
   belongs_to :user
   validates_presence_of :title, :description, :solution, :user_id, :github_project_id
   validates_uniqueness_of :title
+  
+  #Used with the pagination gem that is installed
   cattr_reader :per_page
-  @@per_page = 10
+  @@per_page = 10 #Sets the number of entries per page. 
 
 
+  # The following methed is used to find entries that match a query that is passed to it through the parameters. 
   def self.find_by_query(params={})
     matched_entries = Array.new   
-    log_book_entries = LogBookEntry.find(:all, :joins => [:user, :github_project], :order => params[:order], :include =>[:user, :github_project]) 
-    log_book_entries.each do |entry|
+    log_book_entries = LogBookEntry.find(:all, :joins => [:user, :github_project], :order => params[:order], :include =>[:user, :github_project]) #includes reduce number of sql queries run. 
+    log_book_entries.each do |entry| #creates a large string that holds all entries in the particular entry
       testString = String.new
       testString += entry.user.name
       testString += entry.github_project.github_name
@@ -23,8 +28,8 @@ class LogBookEntry < ActiveRecord::Base
       if testString[params[:query]]
         matched_entries << entry 
       end
-
     end
+  
     matched_entries
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   validates_presence_of :name, :email
   validates_uniqueness_of :name, :email
 
+  #Used in views to let user select the User by name and not id
   def self.options_for_users
     User.find(:all, :order => "name").collect {|u| [u.name, u.id]}  # the way they are aligned ie title first and id later, makes select show title in the view and not id.     
   end
